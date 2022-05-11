7d1adc6a7227acd68b9fa88ae723f0977f238b2f
hardprob <19122634930@163.com>
2022-05-11 13:17:55 +0800

    all

--- jdsc/jdsc/spiders/jd.py
+++ jdsc/jdsc/spiders/jd.py
@@ -72,23 +72,45 @@ class JdSpider(scrapy.Spider):
             self.logger.debug('没了')
         else:
             # 判断上页类型，根据类型确定下页走向
-
             b = re.findall('.*?cat=(.*?)%2C(.*?)%2C(.*?)&page=(.*?)&s=.*', response.url)
-            if (int(b[0][3])>200):
+            c = list(b[0])
+            if (int(c[3])>200):
                 pass
             else:
-                if(int(b[0][3])%2==1):
-                    url='https://list.jd.com/listNew.php?cat={}%2C{}%2C{}&page={}&s={}&scrolling=y&log_id={}&tpl=3_M&isList=1&show_items='.format(b[0][0],b[0][1],b[0][2],int(b[0][3])+1,int(b[0][3])*30-3,time.time()*1000)
-                    header={
-                        'referer':response.url,
-                        'user-agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
-                        'x-requested-with':'XMLHttpRequest',
-                    }
+                while 1:
+                    ex = self.conn.sismember('jdscurls', str(c))
+                    if ex == 1 :
+                        self.log(str(c) + "已经爬取")
+                        c[3]=str(int(c[3])+1)
+                    else:
+                        if (int(c[3]) % 2 == 1):
+                            url = 'https://list.jd.com/listNew.php?cat={}%2C{}%2C{}&page={}&s={}&scrolling=y&log_id={}&tpl=3_M&isList=1&show_items='.format(
+                                c[0], c[1], c[2], int(c[3]) + 1, int(c[3]) * 30 - 3, time.time() * 1000)
+                            header = {
+                                'referer': response.url,
+                                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
+                                'x-requested-with': 'XMLHttpRequest',
+                            }
 
-                    yield scrapy.Request(url, callback=self.parse2, headers=header)
-                else:
-                    url='https://list.jd.com/list.html?cat={}%2C{}%2C{}&page={}&s={}&click=0'.format(b[0][0],b[0][1],b[0][2],int(b[0][3])+1,int(b[0][3])*30-3)
-                    yield scrapy.Request(url, callback=self.parse2)
+                            yield scrapy.Request(url, callback=self.parse2, headers=header)
+
+                        else:
+                            url = 'https://list.jd.com/listNew.php?cat={}%2C{}%2C{}&page={}&s={}&click=0'.format(
+                                c[0], c[1], c[2], int(c[3]) + 1, int(c[3]) * 30 - 3)
+                            yield scrapy.Request(url, callback=self.parse2)
+                        break
+                # if(int(b[0][3])%2==1):
+                #     url='https://list.jd.com/listNew.php?cat={}%2C{}%2C{}&page={}&s={}&scrolling=y&log_id={}&tpl=3_M&isList=1&show_items='.format(b[0][0],b[0][1],b[0][2],int(b[0][3])+1,int(b[0][3])*30-3,time.time()*1000)
+                #     header={
+                #         'referer':response.url,
+                #         'user-agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
+                #         'x-requested-with':'XMLHttpRequest',
+                #     }
+                #
+                #     yield scrapy.Request(url, callback=self.parse2, headers=header)
+                # else:
+                #     url='https://list.jd.com/list.html?cat={}%2C{}%2C{}&page={}&s={}&click=0'.format(b[0][0],b[0][1],b[0][2],int(b[0][3])+1,int(b[0][3])*30-3)
+                #     yield scrapy.Request(url, callback=self.parse2)
 
 
 
